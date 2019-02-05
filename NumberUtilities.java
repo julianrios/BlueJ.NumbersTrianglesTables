d322fc3f07c4efb0f12f9b64eae737ee1b0f84a6
Julian Rios <julianrios@zipcoders-MacBook-Pro.local>
2019-02-05 15:03:27 -0500

    completed all methods and passed all tests

--- NumberUtilities.java
+++ NumberUtilities.java
@@ -4,29 +4,76 @@
 public class NumberUtilities {
 
     public static String getRange(int stop) {
-        return null;
+        String range="";
+        String append;
+        for(int i=0; i < stop; i++) {
+            append = i + "";
+            range+=append;
+            append = "";
+        }
+        return range;
     }
 
     public static String getRange(int start, int stop) {
-        return null;
+        String range="";
+        String append;
+        for(int i=start; i < stop; i++) {
+            append = i + "";
+            range+=append;
+            append = "";
+        }
+        return range;
     }
 
 
     public static String getRange(int start, int stop, int step) {
-        return null;
+        String range="";
+        String append;
+        for(int i=start; i < stop; i+=step) {
+            append = i + "";
+            range+=append;
+            append = "";
+        }
+        return range;
     }
 
     public static String getEvenNumbers(int start, int stop) {
-        return null;
+        String range="";
+        String append;
+        for(int i=start; i < stop; i++) {
+            if((i%2)==0) {
+                append = i + "";
+                range+=append;
+                append = "";    
+            }
+        }
+        return range;
     }
 
 
     public static String getOddNumbers(int start, int stop) {
-        return null;
+        String range="";
+        String append;
+        for(int i=start; i < stop; i++) {
+            if((i%2)!=0) {
+                append = i + "";
+                range+=append;
+                append = "";    
+            }
+        }
+        return range;
     }
 
 
     public static String getExponentiations(int start, int stop, int exponent) {
-        return null;
+        String range="";
+        String append;
+        for(int i=start; i <= stop; i++) {
+            Long output = (long)(Math.pow(i, exponent));
+            append = output + "";
+            range+=append;
+            append = "";    
+        }
+        return range;
     }
 }
