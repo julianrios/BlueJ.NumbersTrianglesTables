663f3bb7e2e6a6bf9a4d97164c898f3f920f9d9c
Julian Rios <julianrios@zipcoders-MacBook-Pro.local>
2019-02-06 19:26:14 -0500

    completed triangles chart

--- TableUtilities.java
+++ TableUtilities.java
@@ -1,15 +1,22 @@
- 
+
 
 public class TableUtilities {
     public static String getSmallMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(5);
     }
 
     public static String getLargeMultiplicationTable() {
-        return null;
+        return getMultiplicationTable(10);
     }
 
     public static String getMultiplicationTable(int tableSize) {
-        return null;
+     String table= "";
+     for (int a = 1; a <= tableSize ; a++) {
+         for (int b = 1; b <= tableSize; b++ ) {
+                table+= String.format("%3d |", a*b);
+         }
+         table += "\n";
+     }
+     return table;
     }
 }
